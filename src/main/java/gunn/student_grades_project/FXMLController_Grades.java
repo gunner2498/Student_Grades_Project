693ba6e80a4f303566d00ef1fd15d88e53d4301a
g.gunn <g.gunn@localhost>
2021-11-25 14:26:12 -0500

    Finished interface, code currently is bad

--- src/main/java/gunn/student_grades_project/FXMLController_Grades.java
+++ src/main/java/gunn/student_grades_project/FXMLController_Grades.java
@@ -10,17 +10,121 @@ Description; Create a 2D array based grade calculator
 
 import java.net.URL;
 import java.util.ResourceBundle;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.effect.Light;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+
+ 
 
 public class FXMLController_Grades implements Initializable {
+//imports    
+@FXML
+    private AnchorPane APNL_MAIN;
+
+    @FXML
+    private Button BTN_ADD;
+
+    @FXML
+    private Button BTN_CAVG;
+
+    @FXML
+    private Button BTN_EXIT;
+
+    @FXML
+    private Button BTN_LIST;
+
+    @FXML
+    private Button BTN_SAVG;
+
+    @FXML
+    private Label LBL_NAME1;
+
+    @FXML
+    private Label LBL_NAME2;
+
+    @FXML
+    private Label LBL_TEST1;
+
+    @FXML
+    private Label LBL_TEST2;
+
+    @FXML
+    private Label LBL_TEST3;
+
+    @FXML
+    private Label LBL_TEST4;
+
+    @FXML
+    private Label LBL_TITLE;
+
+    @FXML
+    private TextArea TXTA_OUTPUT;
+
+    @FXML
+    private TextField TXTF_NAME1;
+
+    @FXML
+    private TextField TXTF_NAME2;
+
+    @FXML
+    private TextField TXTF_TEST1;
+
+    @FXML
+    private TextField TXTF_TEST2;
+
+    @FXML
+    private TextField TXTF_TEST3;
+
+    @FXML
+    private TextField TXTF_TEST4;   
+    
+    
+    //Variables
+    int numStudent = 0;
+    
+    
+    
+    
+    
+    double[ ][ ] Class = new double[5][14];
+    
+    void add() {
+    
+    }
+    
+    void logic() { 
+       for (int i = 0; i < numStudent; i++){
+        Class[i][0]
+           
+           numStudent++; 
+       }
+    }
+    
+    @FXML
+    void addClick(MouseEvent event) {
+        
+    }
+
+    @FXML
+    void exitClick(MouseEvent event) {
+       System.exit(0);
+    }
+    
+    
+    
+
+    
     
 
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        
     }    
 }
